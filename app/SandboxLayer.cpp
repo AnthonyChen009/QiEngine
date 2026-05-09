1b065f393004622faaaf8abf67e90abe03f74fa7
AnthonyChen009 <chen.anthony009@gmail.com>
2026-05-08 18:30:48 -0700

    entt config

--- app/SandboxLayer.cpp
+++ app/SandboxLayer.cpp
@@ -15,24 +15,27 @@ void SandboxLayer::onDetach() {
 }
 
 void SandboxLayer::onUpdate(Qi::Timestep ts) {
-    // move
     m_quad.position += m_quad.velocity * (float)ts;
-    //m_quad.rotation += 90.0f * ts;
 
-    // get window bounds
     float halfW = 1280.0f / 2.0f;
     float halfH = 720.0f / 2.0f;
     float halfSizeX = m_quad.size.x / 2.0f;
     float halfSizeY = m_quad.size.y / 2.0f;
 
-    // bounce
-    if (m_quad.position.x + halfSizeX >= halfW || m_quad.position.x - halfSizeX <= -halfW) {
+    if (m_quad.position.x + halfSizeX >= halfW) {
         m_quad.velocity.x *= -1.0f;
-
+        m_quad.position.x = halfW - halfSizeX;  // clamp
+    } else if (m_quad.position.x - halfSizeX <= -halfW) {
+        m_quad.velocity.x *= -1.0f;
+        m_quad.position.x = -halfW + halfSizeX; // clamp
     }
-    if (m_quad.position.y + halfSizeY >= halfH || m_quad.position.y - halfSizeY <= -halfH) {
-        m_quad.velocity.y *= -1.0f;
 
+    if (m_quad.position.y + halfSizeY >= halfH) {
+        m_quad.velocity.y *= -1.0f;
+        m_quad.position.y = halfH - halfSizeY;  // clamp
+    } else if (m_quad.position.y - halfSizeY <= -halfH) {
+        m_quad.velocity.y *= -1.0f;
+        m_quad.position.y = -halfH + halfSizeY; // clamp
     }
 
     Qi::Renderer2D::drawQuad(m_quad.position, m_quad.size, m_quad.rotation, m_quad.color);
