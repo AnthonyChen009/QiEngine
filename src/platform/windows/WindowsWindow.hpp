d233fdd9fec421a9cb687d5baa159d6a25ef8715
AnthonyChen009 <chen.anthony009@gmail.com>
2026-05-24 02:22:55 -0700

    windows support

---
+++ src/platform/windows/WindowsWindow.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include "core/Window.hpp"
+#include <GLFW/glfw3.h>
+#include <string>
+
+namespace Qi {
+
+class WindowsWindow : public Window {
+public:
+    WindowsWindow(const WindowProps& props);
+    ~WindowsWindow();
+
+    void onUpdate() override;
+
+    unsigned int getWidth() const override { return m_data.width; }
+	unsigned int getHeight() const override { return m_data.height; }
+	std::string getWindowName() const override {return m_data.title;}
+
+	void setEventCallback(const EventCallbackFn& callback) override { m_data.eventCallback = callback; }
+    void setVSync(bool enabled) override;
+    bool isVSync() const override;
+    void waitForValidFramebufferSize() override;
+    void* getNativeWindow() const override { return m_window; }
+
+private:
+    void init(const WindowProps& props);
+    void shutdown();
+
+private:
+    GLFWwindow* m_window = nullptr;
+
+    struct WindowData {
+		std::string title = "Qi Engine";
+		unsigned int width = 1280;
+		unsigned int height = 720;
+		GraphicsAPI graphicsAPI = GraphicsAPI::Vulkan;
+		bool VSync = false;
+
+		EventCallbackFn eventCallback;
+	};
+
+    WindowData m_data;
+
+};
+
+}
